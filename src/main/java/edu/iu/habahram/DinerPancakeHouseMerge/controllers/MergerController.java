8032f3c42df7ec1ce540264359130f62d644b13e
nrs5 <156202774+nrs5@users.noreply.github.com>
2024-03-27 08:32:50 -0400

    part4 done

---
+++ src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/controllers/MergerController.java
@@ -0,0 +1,55 @@
+package edu.iu.habahram.DinerPancakeHouseMerge.controllers;
+
+import edu.iu.habahram.DinerPancakeHouseMerge.model.MenuItem;
+import edu.iu.habahram.DinerPancakeHouseMerge.repository.CafeRepository;
+import edu.iu.habahram.DinerPancakeHouseMerge.repository.DinerRepository;
+import edu.iu.habahram.DinerPancakeHouseMerge.repository.PancakeHouseRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/merger")
+public class MergerController {
+
+    DinerRepository dinerRepository;
+    PancakeHouseRepository pancakeHouseRepository;
+    CafeRepository cafeRepository;
+
+    public MergerController(DinerRepository dinerRepository
+            ,PancakeHouseRepository pancakeHouseRepository
+            ,CafeRepository cafeRepository) {
+        this.dinerRepository = dinerRepository;
+        this.pancakeHouseRepository = pancakeHouseRepository;
+        this.cafeRepository = cafeRepository;
+    }
+
+    @GetMapping
+    public List<MenuItem> get() {
+        List<MenuItem> menuItems = new ArrayList<>();
+        Iterator<MenuItem> lunchItems =
+                dinerRepository.getTheMenuIterator();
+        while(lunchItems.hasNext()) {
+            menuItems.add(lunchItems.next());
+        }
+
+        Iterator<MenuItem> breakfastItems =
+                pancakeHouseRepository.getTheMenuIterator();
+        while(breakfastItems.hasNext()) {
+            menuItems.add(breakfastItems.next());
+        }
+
+        Iterator<MenuItem> dinnerItems =
+                cafeRepository.getTheMenuIterator();
+        while(dinnerItems.hasNext()) {
+            menuItems.add(dinnerItems.next());
+        }
+        return menuItems;
+    }
+}
--- src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/repository/DinerRepository.java
+++ src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/repository/DinerRepository.java
@@ -1,10 +1,11 @@
 package edu.iu.habahram.DinerPancakeHouseMerge.repository;
 
 import edu.iu.habahram.DinerPancakeHouseMerge.model.DinerMenu;
+import edu.iu.habahram.DinerPancakeHouseMerge.model.Menu;
 import edu.iu.habahram.DinerPancakeHouseMerge.model.MenuItem;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Iterator;
 
 @Repository
 public class DinerRepository {
@@ -13,4 +14,10 @@ public class DinerRepository {
         DinerMenu dinerMenu = new DinerMenu();
         return dinerMenu.getMenuItems();
     }
+
+    public Iterator<MenuItem> getTheMenuIterator() {
+        Menu dinerMenu = new DinerMenu();
+        return dinerMenu.createIterator();
+    }
 }
+
